58f5beb04038e6081d487856a4aef70c3d9c2b08
Fidias <fidiasxalfredoz@gmail.com>
2023-05-10 08:43:25 -0600

    firstContent

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -3,24 +3,15 @@ import Card from 'react-bootstrap/Card';
 export const Main = () => {
     return (
         <>
-        {[,
-            'Dark'
-        ].map((variant) => (
-            <Card
-            bg={variant.toLowerCase()}
-            key={variant}
-            text={variant.toLowerCase() === 'light' ? 'dark' : 'white'}
-            style={{ width: '66rem', height:'30rem', marginBottom: '0rem', paddingBottom: '0px'}}
-            >
-            <Card.Header></Card.Header>
+        <Card bg={'dark'} border="dark" style={{ width: '66rem', height: '40rem', marginBottom:'0rem'}} text='light'>
+        <Card.Header></Card.Header>
             <Card.Body>
-                <Card.Title>Gallery Section</Card.Title>
-                    <Card.Text>
+            <Card.Title>Gallery Section</Card.Title>
+                <Card.Text>
                     
-                    </Card.Text>
+                </Card.Text>
             </Card.Body>
-            </Card>
-        ))}
+        </Card>       
         </>
       );
 }
